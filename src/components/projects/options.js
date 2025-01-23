a00f406e50deb5a4c485413608bddc4e99977249
Vinicius Gama Sá <31103259+fireblaze267@users.noreply.github.com>
2025-01-23 13:56:39 -0300

    Style changes

--- src/components/projects/options.js
+++ src/components/projects/options.js
@@ -1,5 +1,3 @@
-import codeby from "../../assets/codeby.png";
-import hdf from "../../assets/hdf.png";
 import only_notes from "../../assets/only_notes.png";
 import overwatch_clone from "../../assets/overwatch_clone.png";
 import serverDriveUi from "../../assets/server_drive_ui.png";
@@ -19,21 +17,21 @@ export const options = [
     img: only_notes,
     link: "https://only-notes-nine.vercel.app/",
   },
-  {
-    key: 2,
-    title: "HDF",
-    text: "Projeto pessoal para mostrar letras de musica",
-    img: hdf,
-    link: "https://hdf-gamma.vercel.app/",
+  // {
+  //   key: 2,
+  //   title: "HDF",
+  //   text: "Projeto pessoal para mostrar letras de musica",
+  //   img: hdf,
+  //   link: "https://hdf-gamma.vercel.app/",
 
-  },
-  {
-    key: 3,
-    title: "Codeby",
-    text: "Processo seletivo : codeby  ",
-    img: codeby,
-    link: "https://codeby-zeta.vercel.app/",
-  },
+  // },
+  // {
+  //   key: 3,
+  //   title: "Codeby",
+  //   text: "Processo seletivo : codeby  ",
+  //   img: codeby,
+  //   link: "https://codeby-zeta.vercel.app/",
+  // },
   {
     key: 4,
     title: "Server Drive UI",
